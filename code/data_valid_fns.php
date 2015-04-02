4f9a0be1ec31a711c3036ea30fc691e558e6fb16
williamyu1996 <william-ysy@outlook.com>
2015-04-01 21:33:13 -0400

    Create data_valid_fns.php
    Functions checking whether user has filled out the form and entered valid email address.

---
+++ code/data_valid_fns.php
@@ -0,0 +1,20 @@
+function filled_out($form_vars) {
+  // test that each input from form has a value
+  foreach ($form_vars as $key => $value) {
+     if ((!isset($key)) || ($value == '')) {
+        return false;
+     }
+  }
+  return true;
+}
+
+function valid_email($address) {
+  // check an email address is  valid
+  if (ereg("^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$", $address)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+?>
