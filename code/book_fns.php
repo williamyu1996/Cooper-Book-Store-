a644708ece08d325489c54c249856ae232e1f44d
williamyu1996 <william-ysy@outlook.com>
2015-04-01 21:30:00 -0400

    Create book_fns.php
    Functions related book information. Connection with MySQL database to get information about books, calculation of shopping cart.

---
+++ code/book_fns.php
@@ -0,0 +1,106 @@
+<?php
+function calculate_shipping_cost() {
+// shipping price is fixed 45.00$
+  return 45.00;
+}
+
+function get_categories() {
+   // ask database for categories 
+   //MySQL API connection
+   
+   $conn = db_connect();
+   $query = "select catid, catname from categories";
+   $result = @$conn->query($query);
+   if (!$result) {
+     return false;
+   }
+   $num_cats = @$result->num_rows;
+   if ($num_cats == 0) {
+      return false;
+   }
+   $result = db_result_to_array($result);
+   return $result;
+}
+
+function get_category_name($catid) {
+   // ask database for category id
+   $conn = db_connect();
+   $query = "select catname from categories
+             where catid = '".$catid."'";
+   $result = @$conn->query($query);
+   if (!$result) {
+     return false;
+   }
+   $num_cats = @$result->num_rows;
+   if ($num_cats == 0) {
+      return false;
+   }
+   $row = $result->fetch_object();
+   return $row->catname;
+}
+
+
+function get_books($catid) {
+   // ask database for the books in a category
+   if ((!$catid) || ($catid == '')) {
+     return false;
+   }
+
+   $conn = db_connect();
+   $query = "select * from books where catid = '".$catid."'";
+   $result = @$conn->query($query);
+   if (!$result) {
+     return false;
+   }
+   $num_books = @$result->num_rows;
+   if ($num_books == 0) {
+      return false;
+   }
+   $result = db_result_to_array($result);
+   return $result;
+}
+
+function get_book_details($isbn) {
+  // get all details for a particular book
+  if ((!$isbn) || ($isbn=='')) {
+     return false;
+  }
+  $conn = db_connect();
+  $query = "select * from books where isbn='".$isbn."'";
+  $result = @$conn->query($query);
+  if (!$result) {
+     return false;
+  }
+  $result = @$result->fetch_assoc();
+  return $result;
+}
+
+function calculate_price($cart) {
+  // sum total price for all items in shopping cart
+  $price = 0.0;
+  if(is_array($cart)) {
+    $conn = db_connect();
+    foreach($cart as $isbn => $qty) {
+      $query = "select price from books where isbn='".$isbn."'";
+      $result = $conn->query($query);
+      if ($result) {
+        $item = $result->fetch_object();
+        $item_price = $item->price;
+        $price +=$item_price*$qty;
+      }
+    }
+  }
+  return $price;
+}
+
+function calculate_items($cart) {
+  // sum total items in shopping cart
+  $items = 0;
+  if(is_array($cart))   {
+    foreach($cart as $isbn => $qty) {
+      $items += $qty;
+    }
+  }
+  return $items;
+}
+?>
