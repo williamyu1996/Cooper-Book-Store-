f0ef108d859568dba2aee6ff3523dacbfde74714
williamyu1996 <william-ysy@outlook.com>
2015-04-02 14:40:44 -0400

    Create edit_cat_form.php

---
+++ manager-code/edit_cat_form.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once('book_sc_fns.php');
+session_start();
+do_html_header("Edit category");
+if (check_admin_user()) {
+  if ($catname = get_category_name($_GET['catid'])) {
+    $catid = $_GET['catid'];
+    $cat = compact('catname', 'catid');
+    display_category_form($cat);
+  } else {
+    echo "<p>Could not retrieve category details.</p>";
+  }
+  do_html_url("admin.php", "Back to administration menu");
+} else {
+//check whether or not you are the manager of book store.
+  echo "<p>You are not authorized to enter the administration area.</p>";
+}
+do_html_footer();
+
+?>
